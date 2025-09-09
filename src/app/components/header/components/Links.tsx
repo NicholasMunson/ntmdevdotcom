3be2767ccd2f5db7cc94c6d20f20ffac28319e0c
nicholasmunson <nicholas.t.munson@gmail.com>
2025-09-09 14:08:00 -0600

    Added links

---
+++ src/app/components/header/components/Links.tsx
@@ -0,0 +1,9 @@
+export default function Links() {
+  return(
+    <div>
+      <a href="https://github.com/NicholasMunson">Github</a>
+      <a href="https://www.linkedin.com/in/nicholastmunson//">LinkedIn</a>
+      <a href="https://drive.google.com/file/d/1Avkmc58rqC0nWKP4ubNuJH77sKChPYoL/view?usp=drive_link">Resume</a>
+    </div>
+  )
+}
